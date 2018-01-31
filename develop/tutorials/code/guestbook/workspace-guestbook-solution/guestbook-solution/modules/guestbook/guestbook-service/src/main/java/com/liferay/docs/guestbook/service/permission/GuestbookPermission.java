d7f92de88de91a4bcb28a567dc1bcd3526705201
codyhoag <cody.hoag@liferay.com>
2018-01-31 11:25:34 -0500

    LRDOCS-3820 Implement BaseModelPermissionChecker

--- develop/tutorials/code/guestbook/workspace-guestbook-solution/guestbook-solution/modules/guestbook/guestbook-service/src/main/java/com/liferay/docs/guestbook/service/permission/GuestbookPermission.java
+++ develop/tutorials/code/guestbook/workspace-guestbook-solution/guestbook-solution/modules/guestbook/guestbook-service/src/main/java/com/liferay/docs/guestbook/service/permission/GuestbookPermission.java
@@ -10,6 +10,7 @@ import com.liferay.exportimport.kernel.staging.permission.StagingPermissionUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.security.permission.BaseModelPermissionChecker;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
 
 		@Component(
@@ -18,7 +19,7 @@ import com.liferay.portal.kernel.security.permission.PermissionChecker;
 				"model.class.name=" + GuestbookPermission.RESOURCE_NAME
 			}
 		)
-        public class GuestbookPermission {
+        public class GuestbookPermission implements BaseModelPermissionChecker {
 
 			public static final String RESOURCE_NAME =
 				"com.liferay.docs.guestbook.model.Guestbook";
@@ -31,6 +32,15 @@ import com.liferay.portal.kernel.security.permission.PermissionChecker;
                     throw new PrincipalException();
                 }
             }
+
+			@Override
+			public void checkBaseModel(
+					PermissionChecker permissionChecker, long groupId, long primaryKey,
+					String actionId)
+				throws PortalException {
+
+				check(permissionChecker, primaryKey, actionId);
+			}
 
             public static boolean contains(PermissionChecker permissionChecker,
                     long guestbookId, String actionId) throws PortalException,
